d002308dd0f1a435f83d2c81a0796a3aaa344b11
jgang212 <jack.gang@CHIHQ-IPC160.XRTRADING.LOCAL>
2015-10-14 14:35:21 -0500

    problem 20 solution

---
+++ 20.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 14:24:57 2015
+
+@author: jack.gang
+"""
+
+import time
+
+def factorialSum(n):
+    new = 1
+    for x in range(1, n+1):
+        new = x * new
+    final= 0
+    while new:        
+        digit = new % 10
+        new = new / 10
+        final += digit
+    return final
+    
+start = time.time()
+
+answer = factorialSum(100)
+
+elapsed = time.time() - start
+
+print("%s found in %s seconds") % (answer,elapsed)
+ 
