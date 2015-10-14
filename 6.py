b5bd8adaa5897d8efaa45e6d2432fce8188a7f3a
jgang212 <jack.gang@CHIHQ-IPC160.XRTRADING.LOCAL>
2015-10-14 15:01:25 -0500

    solution for #6

---
+++ 6.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 14:59:41 2015
+
+@author: jack.gang
+"""
+
+import time
+
+def sumOfSquares(n):
+    totalSum = 0
+    for x in range(1,n+1):
+        totalSum += x*x
+    return totalSum
+
+def squareOfSum(n):
+    totalSum = 0
+    for x in range(1,n+1):
+        totalSum += x
+    return totalSum*totalSum
+
+start = time.time()
+
+answer = squareOfSum(100) - sumOfSquares(100)
+
+elapsed = time.time() - start
+
+print("%s found in %s seconds") % (answer,elapsed)
