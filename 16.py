382e307122825cb885ce7d9af91c5f907c554258
jgang212 <jack.gang@CHIHQ-IPC160.XRTRADING.LOCAL>
2015-10-14 15:59:29 -0500

    solution for #16

---
+++ 16.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 15:58:33 2015
+
+@author: jack.gang
+"""
+
+import time
+
+def sumOfDigits(n):
+    str_n = str(n)
+    totalSum = 0
+    for x in range(0, len(str_n)):
+        totalSum = totalSum + int(str_n[x])
+    return totalSum
+
+start = time.time()
+
+answer = sumOfDigits(2**1000)
+
+elapsed = time.time() - start
+
+print("%s found in %s seconds") % (answer,elapsed)
