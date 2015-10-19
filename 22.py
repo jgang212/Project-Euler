3665cbb53ff00d677156b474e2ea7fdaf3bcff67
jgang212 <jack.gang@CHIHQ-IPC160.XRTRADING.LOCAL>
2015-10-19 13:36:08 -0500

    solutions for #22 and #35

---
+++ 22.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 12:23:13 2015
+
+@author: jack.gang
+"""
+
+import time
+import string
+
+start = time.clock()
+
+wordList = []
+with open('22_names.txt', 'r') as source:
+    for line in source:
+        fields = line.split(',')
+        wordList.append(fields)
+wordList = wordList[0]
+
+values = dict()
+for index, letter in enumerate(string.ascii_uppercase):
+   values[letter] = index + 1
+
+wordList.sort()
+
+totalScore = 0
+for index, word in enumerate(wordList):
+    wordSum = 0
+    word = word.replace('"', "")
+    for letter in word:
+        wordSum = wordSum + values[letter]
+    totalScore = totalScore + (index + 1) * wordSum
+
+answer = totalScore
+
+elapsed = time.clock() - start
+
+print("%s found in %s seconds") % (answer,elapsed)
---
+++ 35.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 13:33:19 2015
+
+@author: jack.gang
+"""
+
+import time    
+
+def isPrime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    limit = int(n**0.5)
+    for x in range(2, limit+1):
+        if n % x == 0:
+            return False
+    return True
+
+def rotate(n):
+    return str(n)[len(str(n))-1] + str(n)[:len(str(n))-1]
+
+start = time.clock()
+
+count = 0
+for x in range(100, 1000000):
+    stop = False
+    if not isPrime(x):
+        continue
+    newX = x
+    for i in range(0, len(str(x))-1):
+        newX = int(rotate(newX))
+        if not isPrime(newX):
+            stop = True
+            continue  
+    if stop:
+        continue
+    #print x
+    count = count + 1
+
+answer = count + 13
+
+elapsed = time.clock() - start
+
+print("%s found in %s seconds") % (answer,elapsed)
