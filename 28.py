75e7e10673fe7a066669b0c441de7e6d0c1a465d
Jack Gang <jack.gang@xrtrading.com>
2017-08-04 13:12:37 -0500

    solution for #28

---
+++ 28.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 13:26:59 2015
+
+@author: jack.gang
+"""
+
+import time    
+
+start = time.clock()
+
+num = 3     # starting number after middle
+size = 1001
+diagonalNums = []
+increment = 2
+for n in range(3,size+1,2):
+    for i in range(0,4):
+        diagonalNums.append((num + increment*i))
+    if (n+1) < size:
+        num = num + increment*4 + 2
+        increment += 2
+
+elapsed = time.clock() - start
+                    
+answer = sum(diagonalNums)+1
+
+print("{} found in {} seconds".format(answer,elapsed))
